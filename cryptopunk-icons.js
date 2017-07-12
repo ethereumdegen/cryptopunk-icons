d1b2e5262ba79a6f709ef4d000352dc2cabc4e6b
andy <andy@jonezie.com>
2017-07-11 21:57:52 -0400

    logs

--- cryptopunk-icons.js
+++ cryptopunk-icons.js
@@ -1,12 +1,20 @@
 module.exports = function() {
 
+
+
+
+		 console.log('booting')
+
+
 	var Web3 = require('web3');
   var web3 = new Web3();
   web3.setProvider(new web3.providers.HttpProvider());
 
 	 var coinbase = web3.eth.coinbase;
 
-	 
+
+	 console.log(coinbase)
+
 
 	var cryptopunks_contract_address = "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB"
 
