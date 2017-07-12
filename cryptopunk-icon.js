9e1d093ded659c9aa41529dd428d5cda6a881be9
andy <andy@jonezie.com>
2017-07-11 21:56:12 -0400

    adding

--- cryptopunk-icon.js
+++ cryptopunk-icon.js
@@ -1,15 +1,16 @@
 module.exports = function() {
 
-
 	var Web3 = require('web3');
+  var web3 = new Web3();
+  web3.setProvider(new web3.providers.HttpProvider());
 
+	 var coinbase = web3.eth.coinbase;
 
-	var web3 = new Web3();
-
+	 
 
 	var cryptopunks_contract_address = "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB"
 
-	var market_abi = "";//read from file 
+	var market_abi = "";//read from file
 
 
 	function getCryptopunkIcon(icon_id)
